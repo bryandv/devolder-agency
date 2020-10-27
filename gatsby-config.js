c054cf05d1d8a49f997192b0cc0b16d9fee83a23
Bryan Devolder <bryan_dv@hotmail.com>
2020-10-27 14:51:01 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      
+      options: {
+      
+      fonts: [`Roboto`, `Oswald`],
+      
+      display: "swap",
+      
+        },
+      }, 
+      `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
